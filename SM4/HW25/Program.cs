4b8fca3dc0d5c1d3a82526f64d425f1b15a8b7f1
galievAA <109428909+galievAA@users.noreply.github.com>
2022-08-22 21:08:27 +0300

    HW25

--- SM4/HW25/Program.cs
+++ SM4/HW25/Program.cs
@@ -1,7 +1,23 @@
-﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+﻿// Задача 25: Напишите цикл, который принимает на вход 
+// два числа (A и B) и возводит число A в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
 
+Console.WriteLine("Введите число А");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B");
+int B = Convert.ToInt32(Console.ReadLine());
+
+
+int number = A;
+
+for (int i = 1; i < B; i++)
+{
+    number = number * A;
+}
+
+Console.WriteLine(" число A в натуральной степени B = " + number);
 
 
