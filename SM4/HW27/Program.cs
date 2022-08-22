2048fdfe472733340bdab50c04b761ec4d93cb5f
galievAA <109428909+galievAA@users.noreply.github.com>
2022-08-22 21:37:42 +0300

    HW27

--- SM4/HW27/Program.cs
+++ SM4/HW27/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 27: Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.WriteLine("Введите число ");
+int A = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+int SumNumber = 0;
+
+while (A > 0)
+{
+    i = A % 10;
+    A = A / 10;
+    SumNumber = SumNumber + i;
+}
+
+Console.WriteLine("Cумма цифр в числе = " + SumNumber);
+
