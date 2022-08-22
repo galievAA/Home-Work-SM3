798bb74316d117d70e79af9c55e26967c37a9948
galievAA <109428909+galievAA@users.noreply.github.com>
2022-08-22 22:13:36 +0300

    HW29

---
+++ SM4/HW29/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 29: Напишите программу, которая задаёт 
+// массив из 8 элементов и выводит их на экран.
+
+Console.WriteLine("Задайте массив из 8 элементов ");
+int i = 0;
+int[] arr = new int [8];
+
+for(i = 0;i < 8;i++)
+{
+    Console.Write(arr[i]);
+}
+
+// Console.WriteLine("Задайте массив из 8 элементов ");
+
+// int[] arr = new int[8];
+// int i = 0;
+// void PrintArray(int[] array)
+// {
+//         int count = array.Length;
+
+//         for(int i = 0; i < count; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+// }
+// void SelectionSort(int[] array)
+// {
+//     for(int i = 0; i < array.Length - 1; i++);
+
+//     int minPosition = i;
+
+//     for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if(array[j] < array[minPosition]) minPosition = j;
+//         }
+
+//     int temporary = array[i];
+//     array[i] = array[minPosition];
+//     array[minPosition] = temporary;
+// }
+
+// PrintArray(arr);
+// SelectionSort(arr);
+// PrintArray(arr);
+
