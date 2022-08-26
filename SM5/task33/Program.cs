1e2f0f3b4a341456ec9be948c13756ffe82ff787
galievAA <109428909+galievAA@users.noreply.github.com>
2022-08-26 22:39:50 +0300

    SM5/task

--- SM5/task33/Program.cs
+++ SM5/task33/Program.cs
@@ -5,3 +5,46 @@
 // Выводить сообщение "да" или "нет" ТОЛЬКО ОДИН РАЗ
 
 
+int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+//                                 10         -10             10
+{
+    int[] array = new int[size];
+
+    Random random = new Random();
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = random.Next(leftRange, rightRange + 1);
+    }
+    return array;
+}
+int[] arr = FillArrayWithRandomNumbers(10, -10, 10);
+Console.WriteLine(string.Join(", ", arr)); // выводим исходный массив
+
+System.Console.WriteLine("Какое число вы хотите найти в массиве? ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool result = IsNumberInArray(arr, number);
+if(result == true)
+{
+    Console.WriteLine("Да");
+}
+
+else
+{
+    Console.WriteLine("Нет");
+}
+
+bool IsNumberInArray(int[] array, int findNumber)
+{
+    bool flag = false; // если елемента в массиве нет, то false, иначе - true
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == findNumber)
+        {
+            flag = true; //  искомый елемент найден
+            break;
+        }
+    }
+
+    return flag;
+}
