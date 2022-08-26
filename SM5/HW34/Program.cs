cc898e57495f36c106a4592cb71f555ff2377fa2
galievAA <109428909+galievAA@users.noreply.github.com>
2022-08-26 23:06:01 +0300

    SM5/HW

--- SM5/HW34/Program.cs
+++ SM5/HW34/Program.cs
@@ -2,5 +2,38 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
 
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Массив: ");
+
+PrintArray(numbers);
+int count = 0;
+
+for (int i = 0; i < numbers.Length; i++)
+if (numbers[i] % 2 == 0)
+count++;
+
+Console.WriteLine($"Колличество {numbers.Length} чисел, {count} из них чётных");
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+
+void PrintArray(int[] numbers)
+{
+    Console.Write("( ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write(") ");
+    Console.WriteLine();
+}
 
