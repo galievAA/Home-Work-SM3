a842de7feebf99c1def5b084a77e108a3f0842b8
galievAA <109428909+galievAA@users.noreply.github.com>
2022-08-28 00:08:48 +0300

    SM5/HW

--- SM5/HW38/Program.cs
+++ SM5/HW38/Program.cs
@@ -1,3 +1,31 @@
 ﻿// Задача 38: Задайте массив вещественных чисел. 
 // Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
+
+double[] arrNumbers = new double[10];
+  for (int i = 0; i < arrNumbers.Length; i++ )
+  {
+    arrNumbers[i] = new Random().Next(1, 10);
+    Console.Write(arrNumbers[i] + " ");
+  }
+
+double maxNumber = arrNumbers[0];
+double minNumber = arrNumbers[0];
+
+  for (int i = 1; i < arrNumbers.Length; i++)
+  {
+    if (maxNumber < arrNumbers[i])
+    {
+      maxNumber = arrNumbers[i];
+    }
+        if (minNumber > arrNumbers[i])
+    {
+      minNumber = arrNumbers[i];
+    }
+  }
+
+  double decision = maxNumber - minNumber;
+  
+  Console.WriteLine("");
+  Console.WriteLine($"Разница между максимальным элементом ({maxNumber}) и минимальным элементом ({minNumber}) = {decision}");
+
