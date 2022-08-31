43a0ffc28322f5bcea6eda6a0d5911d20d1732eb
galievAA <109428909+galievAA@users.noreply.github.com>
2022-08-31 23:59:38 +0300

    SM6/HW

---
+++ SM6/task42/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+GetBinaryView(N);
+
+void GetBinaryView(int number)
+{
+    // рекурсия ( главное правило работы с рекурсией это ее остановка)
+    if (number <= 0) return;
+    GetBinaryView(number / 2);
+    Console.Write(number % 2);
+}
