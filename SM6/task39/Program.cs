7ca5468e6812958b2a30b19817fa3ba877ff2be9
galievAA <109428909+galievAA@users.noreply.github.com>
2022-08-31 23:27:52 +0300

    SM6/HW

--- SM6/task39/Program.cs
+++ SM6/task39/Program.cs
@@ -3,6 +3,15 @@
 // [1 2 3 4 5] -> [5 4 3 2 1]
 // [6 7 3 6] -> [6 3 7 6]
 
+const int ARRAY_SIZE = 6;
+const int LEFT_RANGE = -10;
+const int RIGHT_RANGE = 12;
+
+
+int[] array = FillArrayWithRandomNumbers(ARRAY_SIZE, LEFT_RANGE, RIGHT_RANGE);
+Console.WriteLine($" Исходный массив: {String.Join(", ", array)}");
+ReverseArray(array);
+Console.WriteLine($" Перевернутый массив: {String.Join(", ", array)}");
 
 // метод возвращает массив на size  элементов
 int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
@@ -16,4 +25,14 @@ int[] FillArrayWithRandomNumbers(int size, int leftRange, int rightRange)
         array[i] = random.Next(leftRange, rightRange + 1);
     }
     return array;
+}
+
+void ReverseArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+{
+    int temp = array[i];
+    array[i] = array[array.Length - 1 - i];
+    array[array.Length - 1 - i] = temp;
+}
 }
---
+++ SM6/task40/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 40: Напишите программу, которая принимает на вход три числа и проверяет,
+// может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+Console.WriteLine("Введите сторону A");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите сторону B");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите сторону C");
+int C = Convert.ToInt32(Console.ReadLine());
+
+if (A < B + C && B < A + C && C < A + B)
+{
+    System.Console.WriteLine("Такой треугольник может существовать ");
+}
+else
+{
+    System.Console.WriteLine("Треугольника с заданными сторонами не существует");
+}
+
