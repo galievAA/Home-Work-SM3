8526b66c2496ac7241760898001a0406b3907762
galievAA <109428909+galievAA@users.noreply.github.com>
2022-09-01 23:47:15 +0300

    SM6/HW

---
+++ SM6/HW41/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.WriteLine("Введите с клавиатуры M чисел: ");
+int M = Convert.ToInt32(Console.ReadLine());
+
+int[] MassNumbers = new int[M];
+
+void ImputNumbers(int M)
+{
+    for (int i = 0; i < M; i++)
+  {
+    Console.Write($"Введите {i+1} число: ");
+    MassNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+int Comparison (int[] MassNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < MassNumbers.Length; i++)
+  {
+    if(MassNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+ImputNumbers(M);
+
+Console.WriteLine($"Всего чисел больше 0: {Comparison(MassNumbers)} ");
