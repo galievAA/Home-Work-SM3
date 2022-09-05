8fddc2c1932978adc1493983e015e5ac335907db
galievAA <109428909+galievAA@users.noreply.github.com>
2022-09-06 00:06:26 +0300

    SM7/HW

--- SM7/task49/Program.cs
+++ SM7/task49/Program.cs
@@ -11,6 +11,56 @@
 // 5 81 2 9
 // 8 4 2 4
 
+Console.WriteLine("Введите m");
+int m = Convert.ToInt32(Console.ReadLine()); /// колличество строк
 
+Console.WriteLine("Введите n");
+int n = Convert.ToInt32(Console.ReadLine()); /// количество столбцов
+
+int[,]ourMatrix = GetMatrix(m, n, -10, 10);
+PrinMatrix(ourMatrix);
+SqrEvenIndexs(ourMatrix);
+Console.WriteLine();
+PrinMatrix(ourMatrix);
+
+int[,] GetMatrix(int rowsCount, int columsCount,int lefatRange, int rightRange)
+{
+    int[,] matrix = new int[rowsCount, columsCount];
+
+    Random rand = new Random();
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(lefatRange, rightRange);
+        }
+    }
+    return matrix; 
+}
+void PrinMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+void SqrEvenIndexs(int[,] matrix)
+{
+     for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i % 2 == 0 && j % 2 == 0)
+            {
+                matrix[i, j] = matrix [i, j] * matrix[i, j];
+            }
+        }
+    }
+    return;
+}
 
 
--- SM7/task51/Program.cs
+++ SM7/task51/Program.cs
@@ -6,4 +6,42 @@
 // 8 4 2 4
 // Сумма элементов главной диагонали: 1+9+2 = 12
 
+Console.WriteLine("Введите m");
+int m = Convert.ToInt32(Console.ReadLine()); /// колличество строк
 
+Console.WriteLine("Введите n");
+int n = Convert.ToInt32(Console.ReadLine()); /// количество столбцов
+
+int[,]ourMatrix = GetMatrix(m, n, -10, 10);
+PrinMatrix(ourMatrix);
+
+int[,] GetMatrix(int rowsCount, int columsCount,int lefatRange, int rightRange)
+{
+    int[,] matrix = new int[rowsCount, columsCount];
+
+    Random rand = new Random();
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.WriteLine(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int GetSumMainDiagonal(int[,] matrix)
+{
+    int sum = 0;
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i == j)
+            {
+                sum = sum + matrix[i, j];
+            }
+        }
+        Console.WriteLine(sum);
+        return;
+    }
+}
