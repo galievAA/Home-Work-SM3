244f1a495ca7679732fef7c74e099320c704b201
galievAA <109428909+galievAA@users.noreply.github.com>
2022-09-09 19:08:36 +0300

    SM8/HW

---
+++ SM8/task53/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 53: Задайте двумерный массив. 
+// Напишите программу, которая поменяет местами первую и последнюю строку массива.
+
+int[,] GetMatrix(int rowsCount, int columsCount,int lefatRange, int rightRange)
+{
+    int[,] matrix = new int[rowsCount, columsCount];
+
+    Random rand = new Random();
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(leftRange, rightRange);
+        }
+    }
+    return matrix; 
+}
+void PrinMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SwapFirstAndLastRowa(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(1); i++)
+    {
+        int temp = matrix[0, i];
+        matrix[0, i] = matrix[matrix.GetLength(0) - 1, i];
+        matrix[matrix.GetLength(0) - 1, i]= temp;
+    }
+}
+
+int[,] ourMatrix = GetMatrix(3, 4 -10, 10);
+PrinMatrix(ourMatrix);
+SwapFirstAndLastRowa(ourMatrix);
+Console.WriteLine();
+PrinMatrix(ourMatrix);
